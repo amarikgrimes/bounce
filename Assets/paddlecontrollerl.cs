11394677bef84f675d79f1e799693e019e808c31
Amari Grimes <amarigrimes@spelman.edu>
2024-02-07 10:01:36 -0500

    Initial commit

---
+++ Assets/paddlecontrollerl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddlecontrollerl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0f, 0.007f, 0f);
+            if (transform.position.y > 2.5f)
+            {
+
+                transform.position = new Vector3(transform.position.x, 2.5f, 0f);
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0f, -0.007f, 0f);
+            if (transform.position.y < -2.5f)
+            {
+
+                transform.position = new Vector3(transform.position.x, -2.5f, 0f);
+            }
+
+        }
+    }
+}
